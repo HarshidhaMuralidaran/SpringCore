9df8306d2a45c8db6a7fc8ad7e0030e7c533e54b
HARSHIDHA <HARSHIDHA@LAPTOP-GOEFA9UR>
2025-08-08 13:55:44 +0530

    Project

---
+++ spring-core-1/src/main/java/com/tnsif/spring_core_1/App.java
@@ -0,0 +1,17 @@
+package com.tnsif.spring_core_1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class App {
+    public static void main(String[] args ) {
+    	
+    	ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+    	
+    	vechicle vechicle= (vechicle)context.getBean("vechicleBean");
+    	vechicle.ride();
+    			
+    	}
+    }
+    
---
+++ spring-core-1/src/main/java/com/tnsif/spring_core_1/Bike.java
@@ -0,0 +1,12 @@
+package com.tnsif.spring_core_1;
+
+public class Bike implements vechicle {
+
+	@Override
+	public void ride() {
+		// TODO Auto-generated method stub
+		System.out.println("Bike is riding...");
+	}
+	
+
+}
